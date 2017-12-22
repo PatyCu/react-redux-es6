08a9c78e26a8d7b2c2cb8c5eaf9602cb5625b2ae
PatyCu <patycuenca@gmail.com>
2017-12-22 20:06:40 +0100

    added thunks to handle calls to a mock API to load courses

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,14 +2,8 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
     switch(action.type) {
-        case types.CREATE_COURSE:
-            // NOT IMMUTABLE!
-            //state.push(action.course);
-            //return state;
-            //spread the array, and creates a deep copy of a new state with the new course
-            return [...state,
-                Object.assign({}, action.course)
-            ];
+        case types.LOAD_COURSES_SUCCESS:
+            return action.courses;
         default:
             return state;
     }
